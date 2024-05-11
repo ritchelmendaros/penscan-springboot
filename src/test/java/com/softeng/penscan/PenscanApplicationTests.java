6498e0df7ece32313ac699bfb221a72671f965ea
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-11 18:20:54 +0800

    first commit

---
+++ src/test/java/com/softeng/penscan/PenscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softeng.penscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
