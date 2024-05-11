9d6e1e4308ce70e2d1a302c3cc9102de85f9fc2b
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-12 01:37:02 +0800

    added Student model

---
+++ src/main/java/com/softeng/penscan/model/Student.java
@@ -0,0 +1,19 @@
+package com.softeng.penscan.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "Student")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Student {
+
+    @Id
+    private String studentid;
+    private String userid;
+    private String classesid;
+}
