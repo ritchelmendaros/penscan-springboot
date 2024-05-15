ba91a65323681cd38857f3faa9b681490b5c198d
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-15 13:51:25 +0800

    added model for Item Analysis

---
+++ src/main/java/com/softeng/penscan/model/ItemAnalysis.java
@@ -0,0 +1,21 @@
+package com.softeng.penscan.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "Item_Analysis")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ItemAnalysis {
+
+    @Id
+    private String itemanalysisid;
+    private String quizid;
+    private String itemanalysisname;
+    private String itemanalysis;
+}
